1639fff1cd6c09ed38dad7dce0fd5c6e72dce11c
Sajjad Saberi <sajjad.saberi1996@gmail.com>
2023-09-16 13:07:35 +0330

    changes the codes

--- includes/path.php
+++ includes/path.php
@@ -2,7 +2,7 @@
 
 <div class="title-box">
     <div class="icon-box"><i class="fa fa-home"></i></div>
-    <h4>صفحه اصلی</h4>
+    <a href="http://localhost/Panel/views/index.php#" target="_self">صفحه اصلی</a>
     <div class="icon-box2"><i class="fa fa-chevron-left" aria-hidden="true"></i>
     <h4 class="path"><?php echo $_SESSION["path"]; ?></h4>
     </div>
--- views/index.php
+++ views/index.php
@@ -89,7 +89,7 @@ $run_query = $mysqli->query($query_add_user);
 
 <div class="title-box">
     <div class="icon-box"><i class="fa fa-home"></i></div>
-    <a href="http://localhost/Panel" target="_self">صفحه اصلی</a>
+    <a href="http://localhost/Panel/views/index.php#" target="_self">صفحه اصلی</a>
     <div class="icon-box2"><i class="fa fa-chevron-left" aria-hidden="true"></i>
     <h4 class="path"><?php echo $_SESSION["path"]; ?></h4>
     </div>
