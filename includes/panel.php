becfed6c27b5d4d9c63ecfceef16c1daa7b9d00c
Sajjad Saberi <sajjad.saberi1996@gmail.com>
2023-09-19 21:27:32 +0330

    add spiner svg icons

--- includes/panel.php
+++ includes/panel.php
@@ -27,7 +27,7 @@ $run_query = $mysqli->query($query_add_user);
     </li>
     <li class="profile">
         <img src="./images/aks.png" alt="Profile">
-        <div class="online"><i class="fa fa-circle" aria-hidden="true"></i></div>
+        <div class="online"><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></div>
         <span><span><?php echo $_SESSION["admin-name"]; ?></span><span><?php if($_SESSION["admin-managment"] == "full") {echo "مدیر کل";}elseif($_SESSION["admin-managment"] == "manager"){echo "مدیر مجموعه";}else{echo "منشی";}  ?></span></span>
     </li>
     <li>
